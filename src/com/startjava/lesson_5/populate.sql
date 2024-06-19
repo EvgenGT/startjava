5a954dc4b9c56b929c80e945a42c0c5e4830e0ac
EvgenGT <ssbscrib@gmail.com>
2024-06-19 19:22:39 +0500

    Add populate_db

---
+++ src/com/startjava/lesson_5/populate.sql
@@ -0,0 +1,14 @@
+--init DATABASE
+
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+	VALUES
+		('Cherno Alpha', 	'Mark 1', 	'85', 	'54', 		'Destroyed', 	'Russia', 		'2015', '6'),
+		('Brawler Yukon', 	'Mark 1',	'84', 	'65',		'Destroyed', 	'USA', 			'2015', '2'),
+		('Coyote Tango', 	'Mark 1', 	'90',	'52',		'Destroyed', 	'Japan',		'2016',	'2'),
+		('Romeo Blue',		'Mark 1',	'77', 	'71',		'Destroyed', 	'USA', 			'2015',	'4'),
+		('Tacit Ronin',		'Mark 1',	'74',	'75',		'Destroyed', 	'Japan',		'2017',	'3'),
+		('Crimson Typhoon',	'Mark 4',	'76',	'38',		'Destroyed', 	'China',		'2018',	'7'),
+		('Echo Saber',		'Mark 4',	'87',	'56',		'Destroyed', 	'Japan',		'2018',	'8'),
+		('Nova Hyperion',	'Mark 4',	'65',	'78',		'Destroyed', 	'South Korea',	'2019',	'6'),
+		('Hydra Corinthian','Mark 4',	'79',	'52',		'Destroyed', 	'USA', 			'2018',	'4'),
+		('Mammoth Apostle',	'Mark 4',	'86',	'75',		'Destroyed', 	'Brazil',		'2016',	'4');
